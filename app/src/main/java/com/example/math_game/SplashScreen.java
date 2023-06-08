8e5beae7b648a8dd6e73f0ae878afe35e0835b8e
ACQ06 <questeriaalven@gmail.com>
2023-06-09 07:43:45 +0800

    splash screen adjusted to show members

--- app/src/main/java/com/example/math_game/SplashScreen.java
+++ app/src/main/java/com/example/math_game/SplashScreen.java
@@ -1,18 +1,87 @@
 package com.example.math_game;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashScreen extends AppCompatActivity {
-    private static final long SPLASH_DELAY = 3000; // Delay in milliseconds (e.g., 3000 = 3 seconds)
+
+    private ProgressBar progressBar;
+    private Handler handler;
+    private int progressStatus = 0;
+
+    private String[] names = {"Caber, Jexter Jhon", "Filosopo Ervin Joel", "Pajaron, Jhon Dave", "Questeria, Alven", "Vinoya, Justin Mark"};
+    private String[] roles = {"UI Designer", "Lead Programmer", "App Tester", "Assist. Programmer [Group Leader]", "UX Designer"};
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        androidx.core.splashscreen.SplashScreen.installSplashScreen(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setMax(100);
+
+        TextView name = findViewById(R.id.textName);
+        TextView role = findViewById(R.id.textRole);
+
+        handler = new Handler(Looper.getMainLooper());
+
+        // Create a new thread for the progress bar
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (progressStatus < 100) {
+                    if (progressStatus >= counter * 20 && progressStatus < (counter + 1) * 20) {
+                        final String currentName = names[counter];
+                        final String currentRole = roles[counter];
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                name.setText(currentName);
+                                role.setText(currentRole);
+                            }
+                        });
+                    }
+
+                    progressStatus += 1;
+
+                    // Update the progress bar on the UI thread
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                        }
+                    });
+
+                    try {
+                        // Delay the thread by 100 milliseconds (adjust as needed)
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (progressStatus % 20 == 0) {
+                        counter++;
+                    }
+                }
+            }
+        });
+
+
+
+        // Start the thread
+        thread.start();
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -22,6 +91,6 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(intent);
                 finish(); // Optional: close the splash activity
             }
-        }, SPLASH_DELAY);
+        }, 10000);
     }
 }

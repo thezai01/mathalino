a3a33a6615209a32877ab6c0a64c9b0b33799425
lzumii <ervinfilosopo21@gmail.com>
2023-06-09 09:17:40 +0800

    mathgame

--- app/src/main/java/com/example/math_game/HomePage.java
+++ app/src/main/java/com/example/math_game/HomePage.java
@@ -3,7 +3,10 @@ package com.example.math_game;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
@@ -12,7 +15,7 @@ public class HomePage extends AppCompatActivity {
 
     String username="";
 
-    int intentCheck, lvl, score;
+    int intentCheck, lvl, score, scorelvl1, scorelvl2, scorelvl3;
 
     CardView lvl1, lvl2, lvl3;
 
@@ -29,9 +32,33 @@ public class HomePage extends AppCompatActivity {
         intent = getIntent();
         username = intent.getStringExtra("username");
         intentCheck = intent.getIntExtra("intent",0);
-        if(intentCheck == 1){
-            lvl = intent.getIntExtra("lvl",0);
-            score = intent.getIntExtra("score",0);
+
+        SQLiteDatabase db = openOrCreateDatabase("accDetails", Context.MODE_PRIVATE, null);
+        Cursor cursor = db.rawQuery("SELECT * FROM userScore WHERE username ='" + username + "'", null);
+
+        if(intentCheck == 1) {
+            lvl = intent.getIntExtra("lvl", 0);
+            score = intent.getIntExtra("score", 0);
+
+            switch (lvl) {
+                case 1 -> scorelvl1 = score;
+                case 2 -> scorelvl2 = score;
+                case 3 -> scorelvl3 = score;
+            }
+
+            if (cursor.moveToFirst()) {
+
+                if (!(scorelvl1 > cursor.getInt(1))) scorelvl1 = cursor.getInt(1);
+                if (!(scorelvl2 > cursor.getInt(2))) scorelvl2 = cursor.getInt(2);
+                if (!(scorelvl3 > cursor.getInt(3))) scorelvl3 = cursor.getInt(3);
+
+                db.execSQL("UPDATE userScore SET scorelvl1='" + scorelvl1 + "', scorelvl2='" + scorelvl2 + "', scorelvl3='" + scorelvl3 + "' WHERE username = '" + username + "'");
+            }
+        }else{
+            cursor.moveToFirst();
+            scorelvl1 = cursor.getInt(1) ;
+            scorelvl2 = cursor.getInt(2);
+            scorelvl3 =  cursor.getInt(3);
         }
 
         TextView hpUser = findViewById(R.id.homepageUser);
